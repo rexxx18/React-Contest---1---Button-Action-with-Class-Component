d77f40706bb08a56210baea2379a9a88ae4c16b6
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-10-21 20:30:47 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      paragraph: ""
+    };
+  }
+  handleclick = () => {
+    this.setState({
+      paragraph:
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    });
+  };
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleclick}>
+          click
+        </button>
+        {this.state.paragraph}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
